1a0a4dca15eedbd2e39dd9b7b0f776eb2dc3b11c
it3r4-Achas <47772939+it3r4-Achas@users.noreply.github.com>
2019-03-03 00:24:31 -0800

    Add files via upload

---
+++ templates/admin/header.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap --> 
+    <link rel="stylesheet" type="text/css" href="<?php  echo base_url("assets/vendor/bootstrap/css/bootstrap.min.css"); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/admin/style.css"); ?>">
+
+ 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <title>blog</title>
+      <script>
+        
+  /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+</script>
+
+
+  </head>
+  <body>
+    <div class="container-fluid">
+    
---
+++ templates/admin/nav.php
@@ -0,0 +1,40 @@
+<div id="mySidebar" class="sidebar">
+
+		
+	  	<!---- messages	blogs	list of User/ home 		Logout	 with Profile	--->
+
+  		<a href="javascript:void(0)" class="closebtn" onclick="closeNav()"> &times;</a>
+
+  		<img src="<?php echo base_url("assets/images/kay-vogelgesang.jpg") ?>" class="img-rounded  d-block mx-auto image">
+  		<label class="name"> Erwin P. Rommel </label>
+  		<label class="accountType"> Admin </label>
+
+<div class="links">	
+  <a href="<?php echo base_url("/dbmsblog")?>">User List</a>
+  <a href="<?php echo base_url("/dbmsblog/view")?>">View Articles</a>
+  <a href="#">Publish Article</a>
+  <a href="#">Messages</a>
+  <a href="#">Log-Out</a>
+</div>
+</div>
+<div id="main">
+	  <nav class="mainnavbar sticky-top navbar navbar-expand-lg navbar-light bg-light">
+	  
+
+
+
+
+      <div class="collapse navbar-collapse" id="navbarText">
+    	<ul class="navbar-nav mr-auto">
+      	<li class="nav-item active">
+        	<button class="openbtn" onclick="openNav()">&#9776; Admin </button> 
+     	</li>
+    	</ul>
+    		<span class="navbar-text">
+
+      		Dota2Blog
+
+    		</span>
+		</div>
+
+	 </nav>
