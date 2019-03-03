30ac9d814de4ff36bd8491365964400347518231
it3r4-Villaflor <47772947+it3r4-Villaflor@users.noreply.github.com>
2019-03-03 15:07:39 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,29 @@
+    
+
+
+	<footer>
+		<div class="col-12">
+			<div class="col-6 float-left">
+			<h5>Copyright</h5>
+			</div>
+			<div class="col-6 float-right text-center">
+			<h5>Follow Us</h5>
+			<img src="<?php echo base_url("assets/images/fb.svg") ?>" width="10%">
+			<img src="<?php echo base_url("assets/images/twitter.svg") ?>" width="10%">
+			<img src="<?php echo base_url("assets/images/insta.svg") ?>" width="10%">
+			</div>
+
+		</div>
+	</footer>
+	</div>
+
+  </div>
+   
+  </body>
+
+
+
+    <script>
+</script>
+
+</html>
---
+++ nav.php
@@ -0,0 +1,31 @@
+
+<div id="main" class="row">
+	 <nav class="main navbar sticky-top navbar navbar-expand-lg navbar-light bg-light rounded col-md-12 nav-justified  nav-fill">
+	 <p class="col-md-2">
+	 <img src="<?php echo base_url("assets/images/logo.png") ?>" width="32%">
+	</p>
+    	<ul class="navbar-nav mr-auto float-right col-md-10">
+      	<li class="nav-item col-md-2">
+        	<a class="nav-link " href="#">HOME</a>
+     	 </li>
+      	<li class="nav-item col-md-2">
+        	<a class="nav-link " href="#">ABOUT</a>
+      	</li>
+      	<li class="nav-item col-md-2">
+        	<a class="nav-link " href="#">BLOGS</a>
+      	</li>
+      	<li class="nav-item col-md-2">
+        	<a class="nav-link " href="#">CONTACT US</a>
+      	</li>
+      	<li class="nav-item col-md-2">
+        	<a class="nav-link " href="#">LOGIN</a></button>
+      	</li>
+      	<li class="nav-item">
+      		<small>	
+        		<a class="nav-link" href="#">NOT YET A MEMBER?</a>
+        	</small>
+      	</li>
+    	</ul>
+
+ </nav>
+</div>
