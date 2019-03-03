19cc195454c864f0b67034f2fe70b6b5dff21450
it3r4-Villaflor <47772947+it3r4-Villaflor@users.noreply.github.com>
2019-03-03 16:19:50 +0800

    Add files via upload

--- templates/footer.php
+++ templates/footer.php
@@ -2,13 +2,28 @@
 
 
 	<footer>
-		
-		<p>Coptyright &copy; 2018 | All Rights Reserved</p>
-	
+		<div class="col-12">
+			<div class="col-6 float-left">
+			<h5>Copyright</h5>
+			</div>
+			<div class="col-6 float-right text-center">
+			<h5>Follow Us</h5>
+			<img src="<?php echo base_url("assets/images/fb.svg") ?>" width="10%">
+			<img src="<?php echo base_url("assets/images/twitter.svg") ?>" width="10%">
+			<img src="<?php echo base_url("assets/images/insta.svg") ?>" width="10%">
+			</div>
+
+		</div>
 	</footer>
 	</div>
 
   </div>
    
   </body>
+
+
+
+    <script>
+</script>
+
 </html>
--- templates/nav.php
+++ templates/nav.php
@@ -1,40 +1,31 @@
-<div id="mySidebar" class="sidebar">
 
-		
-	  	<!---- messages	blogs	list of User/ home 		Logout	 with Profile	--->
-
-  		<a href="javascript:void(0)" class="closebtn" onclick="closeNav()"> &times;</a>
-
-  		<img src="<?php echo base_url("assets/images/kay-vogelgesang.jpg") ?>" class="img-rounded  d-block mx-auto image">
-  		<label class="name"> Erwin P. Rommel </label>
-  		<label class="accountType"> Admin </label>
-
-<div class="links">	
-  <a href="<?php echo base_url("/dbmsblog")?>">User List</a>
-  <a href="<?php echo base_url("/dbmsblog/view")?>">View Articles</a>
-  <a href="#">Publish Article</a>
-  <a href="#">Messages</a>
-  <a href="#">Log-Out</a>
-</div>
-</div>
-<div id="main">
-	  <nav class="mainnavbar sticky-top navbar navbar-expand-lg navbar-light bg-light">
-	  
-
-
-
-
-      <div class="collapse navbar-collapse" id="navbarText">
-    	<ul class="navbar-nav mr-auto">
-      	<li class="nav-item active">
-        	<button class="openbtn" onclick="openNav()">&#9776; Admin </button> 
-     	</li>
+<div id="main" class="row">
+	 <nav class="main navbar sticky-top navbar navbar-expand-lg navbar-light bg-light rounded col-md-12 nav-justified  nav-fill">
+	 <p class="col-md-2">
+	 <img src="<?php echo base_url("assets/images/logo.png") ?>" width="32%">
+	</p>
+    	<ul class="navbar-nav mr-auto float-right col-md-10">
+      	<li class="nav-item col-md-2">
+        	<a class="nav-link " href="#">HOME</a>
+     	 </li>
+      	<li class="nav-item col-md-2">
+        	<a class="nav-link " href="#">ABOUT</a>
+      	</li>
+      	<li class="nav-item col-md-2">
+        	<a class="nav-link " href="#">BLOGS</a>
+      	</li>
+      	<li class="nav-item col-md-2">
+        	<a class="nav-link " href="#">CONTACT US</a>
+      	</li>
+      	<li class="nav-item col-md-2">
+        	<a class="nav-link " href="#">LOGIN</a></button>
+      	</li>
+      	<li class="nav-item">
+      		<small>	
+        		<a class="nav-link" href="#">NOT YET A MEMBER?</a>
+        	</small>
+      	</li>
     	</ul>
-    		<span class="navbar-text">
 
-      		Dota2Blog
-
-    		</span>
-		</div>
-
-	 </nav>
+ </nav>
+</div>
