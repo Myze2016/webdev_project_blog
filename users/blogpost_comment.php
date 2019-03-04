480bff1e272f3a470a77e8756d2bbf12683545bf
it3r4-Achas <47772939+it3r4-Achas@users.noreply.github.com>
2019-03-04 05:09:00 -0800

    Add files via upload

---
+++ users/blogpost_comment.php
@@ -0,0 +1,22 @@
+<div class="row postblog">
+<style type="text/css"> 
+.postblog{
+	padding-top: 50px;
+}
+textarea {
+   resize: none;
+   height:50px;
+   width: 100%;
+ 
+</style>
+ <div class="col-md-3">
+   	<img src="<?php echo base_url("assets/images/logo.png") ?>" width="32%">
+ </div>
+  <div class="col-md-9">
+   	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+ </div>
+ <div class="form-group">
+  <label for="exampleFormControlTextarea3">comment: </label>
+  <textarea class="form-control" rows="7" cols="180"></textarea>
+</div>
+</div>
